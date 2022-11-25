f87c1e08b511202e75c6f56c0ccca5db983bcc0f
soonbee <maru204@gmail.com>
2022-11-25 15:43:18 +0900

    Change todo api function from async to sync

--- template/src/apis/todo.js
+++ template/src/apis/todo.js
@@ -2,31 +2,22 @@ import apiClient from "./apiClient";
 
 export async function createTodo(params) {
   const { task } = params;
-  const response = await apiClient.post("/api/v1/todos", { task });
-  return response.data;
+  return apiClient.post("/api/v1/todos", { task });
 }
 
-// export async function getTodos() {
-//   const response = await apiClient.get("/api/v1/todos");
-//   return response.data;
-// }
 export function getTodos() {
   return apiClient.get("/api/v1/todos");
-  // return response.data;
 }
 
-export async function getTodo(todoId) {
-  const response = await apiClient.get(`/api/v1/todos/${todoId}`);
-  return response.data;
+export function getTodo(todoId) {
+  return apiClient.get(`/api/v1/todos/${todoId}`);
 }
 
-export async function updateTodoStatus(params) {
+export function updateTodoStatus(params) {
   const { todoId, status } = params;
-  const response = await apiClient.put(`/api/v1/todos/${todoId}`, { status });
-  return response.data;
+  return apiClient.put(`/api/v1/todos/${todoId}`, { status });
 }
 
 export async function deleteTodo(todoId) {
-  const response = await apiClient.delete(`/api/v1/todos/${todoId}`);
-  return response.data;
+  return apiClient.delete(`/api/v1/todos/${todoId}`);
 }
